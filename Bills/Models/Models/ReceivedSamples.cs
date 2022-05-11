ae81c8f616cf533d8d39afdc5dc26c5f31d5fd8e
Eng-ABDO <Eng-ABDO@DESKTOP-F2MND14>
2022-05-11 05:30:58 +0200

    new

---
+++ Bills/Models/Models/ReceivedSamples.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace DomainLayer.Models
+{
+    public class ReceivedSamples
+    {
+        public int id { get; set; }
+
+        public int receivingSideId { get; set; }
+        public int clientId { get; set; }
+        public DateTime recevingDate { get; set; }
+        
+        [Required()]
+        public string  describtion { get; set; }
+        public int numberOfSamples { get; set; }
+        public string  attachments { get; set; }
+        public string  clienAttachments { set; get; }
+        public decimal sampleCost { set; get; }
+        public bool status { set; get; }
+        public virtual Client client { set; get; }
+        public virtual ReceivingSide side { set; get; }
+
+
+    }
+}
